b9d9d0bd875bffa2bf91164d27ba230bb3ec80e6
TokeloMp <mphonyotl@gmail.com>
2025-09-30 14:21:09 +0200

    Add all project files

---
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.loginapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameInput = findViewById(R.id.username_input);
+        passwordInput = findViewById(R.id.password_input);
+        loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(v -> {
+            String username = usernameInput.getText().toString();
+            String password = passwordInput.getText().toString();
+            Log.i("Test Credential", "Username: " + username + " and Password: " + password);
+        });
+    }
+}
