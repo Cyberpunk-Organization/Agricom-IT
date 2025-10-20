723efcf0a2a1b63fe79a77274bb8dbe486d41e9e
RetroRobinChaos7 <robingraham1204@gmail.com>
2025-10-20 14:58:31 +0200

    Stupid spelling mistake

--- app/src/main/java/com/example/loginapp/ui/SettingsFragment.java
+++ app/src/main/java/com/example/loginapp/ui/SettingsFragment.java
@@ -23,7 +23,7 @@ public class SettingsFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
         view.findViewById(R.id.logout_layout).setOnClickListener(v -> {
             Toast.makeText(getContext(), "Logging out...", Toast.LENGTH_SHORT).show();
