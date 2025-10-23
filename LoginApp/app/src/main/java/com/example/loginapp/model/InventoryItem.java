83e9d5c8db5c41666d86314e4c6a8acce9ea057d
TokeloMp <mphonyotl@gmail.com>
2025-10-23 13:07:21 +0200

    API connection for login/register

---
+++ LoginApp/app/src/main/java/com/example/loginapp/model/InventoryItem.java
@@ -0,0 +1,17 @@
+package com.example.loginapp.model;
+
+public class InventoryItem {
+    private String name;
+    private String category;
+    private int count;
+
+    public InventoryItem(String name, String category, int count) {
+        this.name = name;
+        this.category = category;
+        this.count = count;
+    }
+
+    public String getName() { return name; }
+    public String getCategory() { return category; }
+    public int getCount() { return count; }
+}
