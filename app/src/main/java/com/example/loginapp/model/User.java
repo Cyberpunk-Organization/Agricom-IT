c95025bb5386d196a099e76c9a982d0c3e6c39b5
Dylan Thomas Mathews <167545377+Dylanzo-55@users.noreply.github.com>
2025-10-23 14:23:49 +0200

    commited User.java
    Had to serialized the variables

--- app/src/main/java/com/example/loginapp/model/User.java
+++ app/src/main/java/com/example/loginapp/model/User.java
@@ -1,33 +1,38 @@
 package com.example.loginapp.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class User {
 
     //TODO: integrate with api? Else add sql controls to retrieve data
     //TODO: Get the user roles
+    @SerializedName("userID")
     private int UserID;
+    @SerializedName("Email")
     private String Email;
+    @SerializedName("Name")
     private String Name;
-    private String Password;
+    @SerializedName("Surname")
     private String Surname;
+    @SerializedName("Username")
     private String Username;
 
 
     //-------------------------------------------------------------[Constructor]
     public User() {};
-    public User(int UserID,String Email ,String Name, String Password,String Surname, String Username) {
+    public User(int UserID,String Email ,String Name,String Surname, String Username) {
         this.UserID = UserID;
         this.Email = Email;
         this.Name = Name;
-        this.Password = Password;
         this.Surname = Surname;
         this.Username = Username;
     }
     //---------------------------------------------------------------[getUserID]
-    public int getUserID(){return UserID;}
-    public String getEmail(){return Email;}
-    public String getName(){return Name;}
-    public String getPassword(){return Password;}
-    public String getSurname(){return Surname;}
-    public String getUsername(){return Username;}
+    public int getUserID(){return this.UserID;}
+    public String getEmail(){return this.Email;}
+    public String getName(){return this.Name;}
+
+    public String getSurname(){return this.Surname;}
+    public String getUsername(){return this.Username;}
 
 }
