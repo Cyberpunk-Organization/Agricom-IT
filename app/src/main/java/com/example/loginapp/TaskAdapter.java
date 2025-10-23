83c76860e40dca0373b71035ef7fd24d135d63b6
RetroRobinChaos7 <robingraham1204@gmail.com>
2025-10-23 15:07:23 +0200

    replaced android.r with own context layout

--- app/src/main/java/com/example/loginapp/TaskAdapter.java
+++ app/src/main/java/com/example/loginapp/TaskAdapter.java
@@ -26,7 +26,7 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
     @Override
     public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(android.R.layout.simple_list_item_1, parent, false);
+                .inflate(R.layout.item_task, parent, false);
         return new TaskViewHolder(view);
     }
     private void updateTaskAppearance(@NonNull TaskViewHolder holder, boolean isDone) {
@@ -43,7 +43,7 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
     public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
         Task task = taskList.get(position);
         holder.tvTaskDesc.setText(task.GetDescription());
-        holder.checkBox.setOnCheckedChangeListener(null); // Avoid unwanted triggers
+        holder.checkBox.setOnCheckedChangeListener(null);
         holder.checkBox.setChecked(task.getIS_Done());
 
         // Update appearance if already done
@@ -71,14 +71,14 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
     }
 
 
-    static class TaskViewHolder extends RecyclerView.ViewHolder {
+     static class TaskViewHolder extends RecyclerView.ViewHolder {
         TextView tvTaskDesc;
         CheckBox checkBox;
         ImageButton buttonDelete;
 
         public TaskViewHolder(@NonNull View itemView) {
             super(itemView);
-            tvTaskDesc = itemView.findViewById(android.R.id.text1);
+            tvTaskDesc = itemView.findViewById(R.id.tvDescription);
             checkBox = itemView.findViewById(R.id.checkBox);
             buttonDelete = itemView.findViewById(R.id.buttonDelete);
         }
